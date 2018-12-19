5f7b421cfac714450f024d120f041a6b2ae52d3b
Ilene Cohen <ilene413@icloud.com>
2018-12-19 01:13:20 -0500

    input validated

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -22,13 +22,15 @@ $(document).ready(function () {
     let trainName;
     let destination;
     let firstTrain = 0;
-    let freqInMIn = 0;
+    let freqInMin = 0;
     let nextArrival;
     let minAway = 0;
     let arrTrack = 0;
     let isItMilitary;
     let timeOfTrain;
     let currentTime;
+    let validform = true;
+    let errormsg;
 
     let trainObj = {
         trainName: "",
@@ -43,6 +45,8 @@ $(document).ready(function () {
     // check that all required fields are entered before submit
 
     $("#submit-train").on("click", function (event) {
+        event.preventDefault();
+
 
         trainName = $("#train-name").val().trim();
         destination = $("#destination").val().trim();
@@ -50,84 +54,159 @@ $(document).ready(function () {
         firstTrain = $("#first-train").val().trim();
         arrTrack = $("#trackNo").val().trim();
 
+        //validate input - if an input field is invalid, will return form with error messages for all invalid fields
+        //if a field is invalid - valid form is set to false
 
+        // check that train name was entered
 
-        //check to see that first-train is a valid military number between 00:00 - 23:59
-        //note max text size of first-train is 5, therefore position 3 should be a :
+        if (trainName == "") {
+            errorMsg = "Please Enter Train Name";
+            document.getElementById("trainNameError").innerHTML = errorMsg;
+            validform = false;
+        }
+        else {
+            clearErrMsg("trainNameError");
+        }
+        // check that destination was entered
+
+        if (destination == "") {
+            errorMsg = "Please Enter Destination";
+            document.getElementById("destinationError").innerHTML = errorMsg;
+            validform = false;
+        }
+        else {
+            clearErrMsg("destinationError");
+        }
+
+        //check to see that first-train is a valid military time between 00:00 - 23:59
+        //note max text size of first-train is 5, therefore position 3 should be a "":"
+
         let timeTrain = moment(firstTrain, "HH:mm");
-        console.log("moment " + timeTrain);
         let militChar = firstTrain.charAt(2);
+        errorMsg = "Invalid - time must be entered in military time";
         if (militChar !== ":") {
-            return;
+            document.getElementById("firstTrainError").innerHTML = errorMsg;
+            validform = false;
         }
         else {
             isItMilitary = firstTrain.substr(0, 2);
             if (isItMilitary < 0 || isItMilitary > 23) {
-                return;
+                document.getElementById("firstTrainError").innerHTML = errorMsg;
+                validform = false;
             }
             else {
                 isItMilitary = parseInt(firstTrain.substr(3, 2));
                 if (isItMilitary < 0 || isItMilitary > 59) {
-                    return;
-
+                    document.getElementById("firstTrainError").innerHTML = errorMsg;
+                    validform = false;
+                }
+                else {
+                    clearErrMsg("firstTrainError");
                 }
             }
         }
-        timeOfTrain = moment(firstTrain, "HH:mm").subtract(1, "years");
-        console.log("time of first train " + moment(timeOfTrain).format("hh:mm"));
-        currentTime = moment(currentTime);
-        console.log("current " + moment(currentTime).format("hh:mm"));
-        let diffTime = moment().diff(moment(timeOfTrain, "minutes"));
-        console.log(moment(diffTime).format("hh:mm"));
-        let tRemainder = diffTime % freqInMin;
-        console.log("remainder " + tRemainder);
-        let minToTrain = freqInMin - tRemainder;
-        console.log("min to train " + minToTrain);
-        //if (moment(currentTime) < timeOfTrain)
-        //  timeOfNextTrain = moment(timeOfTrain).add(minToTrain, "minutes");
-        trainObj.trainName = trainName;
-        trainObj.destination = destination;
-        trainObj.frequency = freqInMin;
-        trainObj.firstTrain = firstTrain;
-        trainObj.track = arrTrack;
+
+        //check that frequency was entered
+        //check to make sure frequency is not entered as an "e" (undefined) as numeric data can be enter an "e" or zero
+        if (isNaN(freqInMin) || freqInMin === "" || freqInMin === "0") {
+            errorMsg = "Please Enter Frequency -  must be at least 1";
+            document.getElementById("frequencyError").innerHTML = errorMsg;
+            validform = false;
+        }
+        else {
+            clearErrMsg("frequencyError");
+        }
+
+        //not required to enter a track number, but if it is must be a number greater than 0
+        if (arrTrack === "0") {
+            errorMsg = "Invalid track number - must be at least 1";
+            document.getElementById("trackError").innerHTML = errorMsg;
+            validform = false;
+        }
+        else {
+            clearErrMsg("trackError");
+        }
+
+        if (validform) {
+            timeOfTrain = moment(firstTrain, "HH:mm").subtract(1, "years");
+            console.log("time of first train " + moment(timeOfTrain).format("hh:mm"));
+            currentTime = moment(currentTime);
+            console.log("current " + moment(currentTime).format("hh:mm"));
+            let diffTime = moment().diff(moment(timeOfTrain, "minutes"));
+            console.log(moment(diffTime).format("hh:mm"));
+            let tRemainder = diffTime % freqInMin;
+            console.log("remainder " + tRemainder);
+            let minToTrain = freqInMin - tRemainder;
+            console.log("min to train " + minToTrain);
+            //if (moment(currentTime) < timeOfTrain)
+            //  timeOfNextTrain = moment(timeOfTrain).add(minToTrain, "minutes");
+            trainObj.trainName = trainName;
+            trainObj.destination = destination;
+            trainObj.frequency = freqInMin;
+            trainObj.firstTrain = firstTrain;
+            trainObj.track = arrTrack;
 
 
-        // Code for the push
-        database.ref("trains").push(trainObj);
+            // Code for the push
+            database.ref("trains").push(trainObj);
 
-        //empty form values
-        $("#train-name").val("");
-        $("#destination").val("");
-        $("#frequency").val("");
-        $("#first-train").val("");
-        $("#trackNo").val("");
+            //empty form values
+            $("#train-name").val("");
+            $("#destination").val("");
+            $("#frequency").val("");
+            $("#first-train").val("");
+            $("#trackNo").val("");
+        }
+        else {
+            validform = true;
+        }
+
     });
-        database.ref("trains").on("child_added", function (snapshot) {
-            trainName = snapshot.val().trainName;
-            destination = snapshot.val().destination;
-            freqInMin = snapshot.val().frequency;
-            nextArrival = snapshot.val().nextArr;
-            minAway = snapshot.val().minutesAway;
-            arrTrack = snapshot.val().track;
+
+    database.ref("trains").on("child_added", function (snapshot) {
+        trainName = snapshot.val().trainName;
+        destination = snapshot.val().destination;
+        freqInMin = snapshot.val().frequency;
+        nextArrival = snapshot.val().nextArr;
+        minAway = snapshot.val().minutesAway;
+        arrTrack = snapshot.val().track;
 
 
-            // Create new row in train schedule
-            let trainRow = $("<tr>").append(
-                $("<td>").text(trainName),
-                $("<td>").text(destination),
-                $("<td>").text(freqInMin),
-                $("<td>").text(nextArrival),
-                $("<td>").text(minAway),
-                $("<td>").text(arrTrack)
-            );
+        // Create new row in train schedule
+        let trainRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(destination),
+            $("<td>").text(freqInMin),
+            $("<td>").text(nextArrival),
+            $("<td>").text(minAway),
+            $("<td>").text(arrTrack)
+        );
 
-            // Append the new row to the table
-            $("#train-table > tbody").append(trainRow);
+        // Append the new row to the table
+        $("#train-table > tbody").append(trainRow);
 
-            // Handle the errors
-        }, function (errorObject) {
-            console.log("Errors handled: " + errorObject.code);
+        // Handle the errors
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
 
-        });
+    });
+
+
+    //this function is used to clear any error messages
+    function clearErrMsg(idName) {
+        errorMsg = "";
+        document.getElementById(idName).innerHTML = errorMsg;
+
+    }
+    function checkForE() {
+        onKeyPress = event.keyCode;
+        if (onKeyPress == "e") {
+            errorMsg = "Please Enter Frequency - must be at least 1";
+            document.getElementById("frequencyError").innerHTML = errorMsg;
+            validform = false;
+            onKeyPress = "";
+
+        }
+    }
 
 });
